fe29306823e108c0cacee2f8ff7f4f67bc573a8c
yuliia-samarska <samarska.julia@gmail.com>
2026-08-27 23:15:40 +0300

    Update side lengths in task_5.py for hypotenuse calculation

--- modules/module_1/task_5.py
+++ modules/module_1/task_5.py
@@ -1,6 +1,7 @@
 #Обчислення гіпотенузи
 
-side_a = 10
-side_b = 5
+side_a = 45
+side_b = 78
 hypotenuse = (side_a**2 + side_b**2)**0.5
-print("Гіпотенуза трикутника зі сторонами" {side_a} "та" {side_b} "дорівнює" {hypotenuse})
+
+print(f"Гіпотенуза трикутника зі сторонами {side_a} та {side_b} дорівнює {hypotenuse}")
