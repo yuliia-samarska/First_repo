51b96aa80d67e5c567793a2b6a894f0f6e8ef122
yuliia-samarska <samarska.julia@gmail.com>
2026-08-27 23:10:14 +0300

    Add task_5.py for hypotenuse calculation with user-defined sides

---
+++ modules/module_1/task_5.py
@@ -0,0 +1,6 @@
+#Обчислення гіпотенузи
+
+side_a = 10
+side_b = 5
+hypotenuse = (side_a**2 + side_b**2)**0.5
+print("Гіпотенуза трикутника зі сторонами" {side_a} "та" {side_b} "дорівнює" {hypotenuse})
